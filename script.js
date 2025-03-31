36c76a072548d1029c9ea2647fccfd8aaf0fb228
vikrantsrgc <145971065+vikrantsrgc@users.noreply.github.com>
2025-03-31 14:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	 return arr.reduce((acc, num) => {
+        let lastSubarray = acc[acc.length - 1];
+        
+        if (!lastSubarray || lastSubarray.reduce((sum, val) => sum + val, 0) + num > n) {
+            acc.push([num]);
+        } else {
+            lastSubarray.push(num);
+        }
+        
+        return acc;
+    }, []);
+
 };
 
 const n = prompt("Enter n: ");
